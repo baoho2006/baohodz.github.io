8242c4fe4affe9227293bf5faf33ac3d26eaa1e0
Bảo Hồ <hophamgiabao4b@gmail.com>
2024-01-22 12:56:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 //注册链接
 const config_registerLinks = [
-    "https://www.4win55.com/?a=87",
-    "https://www.4win55.com/?a=87",
-    "https://www.4win55.com/?a=87",
-    "https://www.4win55.com/?a=87",
-    "https://www.4win55.com/?a=87",
-    "https://www.4win55.com/?a=87"
+    "https://www.4win55.com/?a=3311442",
+    "https://www.4win55.com/?a=3311442",
+    "https://www.4win55.com/?a=3311442",
+    "https://www.4win55.com/?a=3311442",
+    "https://www.4win55.com/?a=3311442",
+    "https://www.4win55.com/?a=3311442"
 ]
 const config_phone1 ='13124220298'//手机号码1
 const config_phone2 = '17645485273'//手机号码2
 //客服地址
 const config_kefu_url = "https://www.win55kf.com"
 //站点跳转地址
-const config_site_url = "https://www.4win55.com/?a=87"
+const config_site_url = "https://www.4win55.com/?a=3311442"
 const api = "https://137722.cc"//服务器API地址
